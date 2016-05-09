b7ff40765c35d61388152fd442561d31dbeec2cf
Toni <Toni@DESKTOP-S7MLKAG>
2016-05-10 01:03:17 +0200

    He posat tres mains

--- src/provagit/ProvaGit.java
+++ src/provagit/ProvaGit.java
@@ -17,5 +17,11 @@ public class ProvaGit {
     public static void main(String[] args) {
         // TODO code application logic herefghjmmrewhjmhrewhjmkhgfd
     }
+    public static void main(String[] args) {
+        // TODO code application logic herefghjmmrewhjmhrewhjmkhgfd
+    }
+    public static void main(String[] args) {
+        // TODO code application logic herefghjmmrewhjmhrewhjmkhgfd
+    }
     
 }
